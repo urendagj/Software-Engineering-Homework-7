24171d71af9566c7948a1af9c3e5113b21454aaf
Jacob Urenda Garibay <urendajacob@gmail.com>
2021-03-07 19:31:39 -0800

    Wrote code to pass all three tests

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,7 +2,9 @@
 def FizzBuzz(n):
     result = []
     for i in range(1, n+1):
-        if i % 3 == 0:
+        if i % 3 == 0 and i % 5 == 0:
+            result.append('fizzbuzz')
+        elif i % 3 == 0:
             result.append('fizz')
         elif i % 5 == 0:
             result.append('buzz')
--- UnitTestFizzBuzz.py
+++ UnitTestFizzBuzz.py
@@ -11,7 +11,6 @@ class TestCase(unittest.TestCase):
         self.assertEqual(FizzBuzz.FizzBuzz(10), ['1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', 'buzz'])
 
     def test_multiple_3and5_should_return_Fizzbuzz(self):
-        #this test will also check to see that all 100 numbers can be output
         self.assertEqual(FizzBuzz.FizzBuzz(15), ['1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', 'buzz', '11', 'fizz', '13', '14', 'fizzbuzz'])
 
 
