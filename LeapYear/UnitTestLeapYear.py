65138855ea29d9baac0575ba131201ec6b79924a
Jacob Urenda Garibay <urendajacob@gmail.com>
2021-03-07 20:40:32 -0800

    Added some extra conditions in tests

--- LeapYear/UnitTestLeapYear.py
+++ LeapYear/UnitTestLeapYear.py
@@ -4,11 +4,15 @@ import Leapyear
 class TestLeapYear(unittest.TestCase):
     def test_is_leap_year(self):
         self.assertTrue(Leapyear.Leapyear(2000))
+        self.assertTrue(Leapyear.Leapyear(2012))
+        self.assertTrue(Leapyear.Leapyear(2020))
         self.assertTrue(Leapyear.Leapyear(2400))
 
     def test_isnot_leap_year(self):
         self.assertFalse(Leapyear.Leapyear(1900))
-        self.assertFalse(Leapyear.Leapyear(2100))
+        self.assertFalse(Leapyear.Leapyear(2007))
+        self.assertFalse(Leapyear.Leapyear(1845))
+        self.assertFalse(Leapyear.Leapyear(2500))
 
 
 
