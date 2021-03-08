4bb05685a555c0b6af3f3a111ff2a7c219d02019
Jacob Urenda Garibay <urendajacob@gmail.com>
2021-03-07 20:02:53 -0800

    Wrote code to pass non leap year UnitTest

--- LeapYear/Leapyear.py
+++ LeapYear/Leapyear.py
@@ -1,6 +1,10 @@
 def Leapyear(year):
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return True
+    if year % 100 == 0:
+        if year % 400 == 0:
+            return True
         else:
             return False
+    #if year is not divisible by 100
+    if year % 4 == 0:
+        return True
+    return False
